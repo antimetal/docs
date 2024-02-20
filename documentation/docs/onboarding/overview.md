ba3dca1838cd5af5ae709f1ede6d98b7b06b561a
Shreyas Iyer <shreyasiyer@college.harvard.edu>
2024-02-20 18:44:39 -0500

    ini

---
+++ documentation/docs/onboarding/overview.md
@@ -0,0 +1,18 @@
+---
+title: Onboarding Overview
+description: Overview of the steps to get onboarded to Antimetal.
+hide_table_of_contents: true
+pagination_next: null
+sidebar_label: Onboarding Overview
+---
+
+# Onboarding Overview
+
+To onboard, you can follow the flow at [groups.antimetal.com](https://groups.antimetal.com). At a high-level, the steps include:
+
+1. Create an account at [groups.antimetal.com](https://groups.antimetal.com)
+2. Grant Antimetal billing-layer access to your AWS account(s) by creating an IAM role
+3. Connect your payment method
+4. Fill out your company’s information as part of our KYC process
+5. Join our organization—this stage may involve destructuring your organization
+6. Start saving!
---
+++ documentation/docusaurus.config.ts
@@ -0,0 +1,117 @@
+import type * as Preset from "@docusaurus/preset-classic";
+import type { Config } from "@docusaurus/types";
+import { themes as prismThemes } from "prism-react-renderer";
+
+const config: Config = {
+  title: "Antimetal Documentation",
+  // tagline: "cool tagline",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://your-docusaurus-site.example.com",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    colorMode: {
+      defaultMode: "light",
+      disableSwitch: true,
+    },
+    navbar: {
+      title: "Antimetal Docs",
+      logo: {
+        alt: "Antimetal Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          href: "https://github.com/antimetal",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "light",
+      // links: [
+      //   {
+      //     title: "Docs",
+      //     items: [
+      //       {
+      //         label: "Tutorial",
+      //         to: "/docs/intro",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "Community",
+      //     items: [
+      //       {
+      //         label: "Twitter",
+      //         href: "https://twitter.com/docusaurus",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "More",
+      //     items: [
+      //       {
+      //         label: "Blog",
+      //         to: "/blog",
+      //       },
+      //       {
+      //         label: "GitHub",
+      //         href: "https://github.com/facebook/docusaurus",
+      //       },
+      //     ],
+      //   },
+      // ],
+      // logo: { alt: "Antimetal", src: "/img/vantage-footer-logo.svg" },
+      copyright: `© ${new Date().getFullYear()} Antimetal Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
